d64f9e4aee88ff29cb1394fa6ef24facae905d87
koala9527 <15549806382@qq.com>
2023-02-12 12:13:15 +0800

    Python 简单完成

--- python-api/app.py
+++ python-api/app.py
@@ -1,27 +1,24 @@
-from flask import Flask,request,jsonify, Response
+from flask import Flask,request,jsonify
 import socket
-import json
 import consul
-from flask_jsonrpc import JSONRPC
-from jsonrpcserver import method, Result, Success, dispatch
 
 PORT = 8000
 CONSUL_PORT = 8500
 HOST = '127.0.0.1'
 
-app = Flask("application")
+
+app = Flask(__name__)
 
 # 注册服务到 Consul
 def register_service():
     cursor = consul.Consul(host=HOST, port=CONSUL_PORT, scheme='http')
     service_address = get_host_ip()  # a better way to get ip
-    # cursor.apply_remote_config(namespace='mynamespace/')
+    print(service_address)
     cursor.agent.service.register(
         name='PythonApiService', address=service_address, port=PORT,
         check=consul.Check().tcp(host=service_address, port=PORT,
                                  interval='5s',
-                                 timeout='30s', deregister='30s'),
-
+                                 timeout='30s', deregister='30s')
     )
 
 def get_host_ip():
@@ -33,37 +30,31 @@ def get_host_ip():
         s.close()
     return ip
 
+# 定义 JSON-RPC 调用的方法
+@app.route('/', methods=['POST'])
+def json_rpc():
+    # 解析 JSON-RPC 请求
+    json_request = request.get_json()
+    method = json_request.get("method")
+    params = json_request.get("params")
+    result = ' from python-api@GetMsg:'+params[0]
+    # 返回 JSON-RPC 响应
+    response = {
+        "jsonrpc": "3.0",
+        "result": result,
+        "id": json_request.get("id")
+    }
+    return jsonify(response), 200
+
+# 定义 JSON-RPC 调用的方法
 @app.route('/check', methods=['GET'])
 def health():
     response = {
         "jsonrpc": "2.0",
     }
-    return jsonify(response)
 
-# jsonrpc = JSONRPC(app, '/')
-@app.route('/', methods=['POST'])
-# @jsonrpc.method(validate=False)
-def getMsg():
-    print(request.json)
-    res = {
-    "jsonrpc": "2.0",
-    "id": "61025bc35e07d",
-    "error": {
-        "code": -32000,
-        "message": "user not found",
-        "data": {
-            "class": "RuntimeException",
-            "code": 0,
-            "message": "user not found"
-        }
-    },
-    "context": []
-}
-    return Response(
-        dispatch(res), content_type="application/json"
-    )
+    return jsonify(response), 200
 
-# {'jsonrpc': '2.0', 'method': '/python_api/getMsg', 'params': ['Im admin'], 'id': '63e798b5306fb', 'context': []}
 if __name__ == '__main__':
     register_service()
     app.run(host='0.0.0.0', port=PORT, debug=True)
